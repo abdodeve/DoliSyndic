2f215c1ea21af54123f227e43dafc7bd3485c05d
abdodeve <abdelhadi.deve@gmail.com>
2018-03-14 14:43:19 +0000

    update fucntion parameter

--- admin/script.js
+++ admin/script.js
@@ -44,12 +44,12 @@ function func_setupCtrl ($scope,$http,$location) {
                                 'Authorization' :'Bearer '+localStorage.token
                         },
            data    :   {
-           				        is_penalite_static 		= $scope.is_penalite_static,		
-				                budget 			   		= $scope.budget,
-				                taux_tantieme	   		= $scope.taux_tantieme,
-				                totale_tantieme	   		= $scope.totale_tantieme,
-				                penalite_static_frais 	= $scope.penalite_static_frais,
-				                penalite_dynamic_taux	= $scope.penalite_dynamic_taux
+           				        is_penalite_static 		: $scope.is_penalite_static,		
+				                budget 			   		: $scope.budget,
+				                taux_tantieme	   		: $scope.taux_tantieme,
+				                totale_tantieme	   		: $scope.totale_tantieme,
+				                penalite_static_frais 	: $scope.penalite_static_frais,
+				                penalite_dynamic_taux	: $scope.penalite_dynamic_taux
            			   }
         });
         req.then(function mySuccess(response) {
